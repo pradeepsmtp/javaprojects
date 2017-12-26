2bd5db2b8a4174301ded48b2422ec6e02ce81979
pradeepsmtp <pradeepsmtp@gmail.com>
2017-12-25 19:00:17 -0500

    change 2

--- sample2/appdemo/src/pa/appdemo/HW.java
+++ sample2/appdemo/src/pa/appdemo/HW.java
@@ -4,7 +4,7 @@ public class HW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Hello again");
+		System.out.println(" Hello again d");
 
 	}
 
