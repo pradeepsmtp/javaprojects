9b333c5a2ce6559a1c89852b66d68e03d2adf1cc
pradeepsmtp <pradeepsmtp@gmail.com>
2018-09-23 19:23:36 -0400

    Soap samples

---
+++ sample2/appdemo/src/pa/soap/client/SoapClient.java
@@ -0,0 +1,23 @@
+package pa.soap.client;
+
+import com.thomas_bayer.blz.BLZService;
+import com.thomas_bayer.blz.BLZServicePortType;
+import com.thomas_bayer.blz.DetailsType;
+
+public class SoapClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BLZService o = new BLZService();
+		BLZServicePortType p = o.getPort(BLZServicePortType.class);
+		DetailsType d =  p.getBank("86010424") ;
+		System.out.println( " 86010424 D " +d.getBezeichnung()  );
+		System.out.println( " D " +d.getBic()  );
+		System.out.println( " D " +d.getOrt()  );
+		System.out.println( " D " +d.getPlz()  );
+		
+		
+	}
+
+}
